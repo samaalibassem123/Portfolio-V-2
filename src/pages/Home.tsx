fc3aaad5d331e393cc998e3f97b26fbcd0d1b0bc
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-05 13:50:15 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,32 +3,23 @@ import HomeSection from "../components/home-section/HomeSection";
 import LeftSide from "../components/home-section/LeftSide";
 import RightSide from "../components/home-section/RightSide";
 import Background from "../components/ui/Background";
-import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <>
-      <div className="bg-black/70 w-full h-full absolute top-0 -z-10 left-0"></div>
-      <motion.div
-        initial={{ scale: 0.9 }}
-        animate={{ scale: 1 }}
-        transition={{ delay: 2, duration: 0.2 }}
-        className="flex flex-col h-full w-full "
+    <div className="flex flex-col h-full w-full ">
+      <Background />
+      <a
+        href="https://github.com/samaalibassem123"
+        target="_blank"
+        className=" absolute underline   lg:inline-block hidden bottom-1/6 -rotate-90 hover:-rotate-0   backdrop-blur-2xl  select-all selec p-7 px-4 z-50  cursor-pointer  w-fit transition-transform tracking-wider text-black font-manrope "
       >
-        <Background />
-        <a
-          href="https://github.com/samaalibassem123"
-          target="_blank"
-          className=" absolute underline   lg:inline-block hidden bottom-1/6 -rotate-90 hover:-rotate-0   backdrop-blur-2xl  select-all selec p-7 px-4 z-50  cursor-pointer  w-fit transition-transform tracking-wider text-black font-manrope "
-        >
-          Follow Me On Github
-        </a>
-        <HomeSection className=" h-full pt-3">
-          <LeftSide />
-          <RightSide />
-        </HomeSection>
-        <Footer />
-      </motion.div>
-    </>
+        Follow Me On Github
+      </a>
+      <HomeSection className=" h-full pt-3">
+        <LeftSide />
+        <RightSide />
+      </HomeSection>
+      <Footer />
+    </div>
   );
 }
