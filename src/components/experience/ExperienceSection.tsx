3fbec63c26c0734f8178d31eab123f59b65b5990
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-05 17:45:00 +0100

    update the experience page

--- src/components/experience/ExperienceSection.tsx
+++ src/components/experience/ExperienceSection.tsx
@@ -3,30 +3,30 @@ import Title from "../ui/Title";
 
 export default function ExperienceSection() {
   return (
-    <div className=" h-fit relative pb-10 z-10 ">
+    <div className=" h-fit relative pb-10">
       <PageTitle>Experience</PageTitle>
-      <div className=" flex sm:flex-row flex-col items-center  w-full z-10 text-black px-7 ">
+      <div className=" flex z-30 sm:flex-row flex-col items-center  w-full z-10 text-black px-7 ">
         {/*Left side */}
         <div className=" flex sm:flex-1  flex-col gap-7 bg-white">
           <Title className=" font-manrope font-bold">
             <a
               href="https://www.addinn-group.com/"
               target="_blank"
-              className=" hover:underline transition-all motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1500"
+              className=" hover:underline transition-all motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1500"
             >
               Addinn,
             </a>
             <br />
-            <span className="motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
+            <span className="motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
               Angular developer
             </span>
           </Title>
-          <span className=" text-sm font-manrope motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
+          <span className=" text-sm font-manrope motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
             Tunis, Northern Urban Centre <br /> July 2025 – Aug 2025 (2 Months)
           </span>
         </div>
         {/*Right side */}
-        <div className=" sm:flex-1 p-2 bg-white motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1700">
+        <div className=" sm:flex-1 p-2 bg-white motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1700">
           <ul className=" list-disc font-manrope space-y-3 text-lg/8 tracking-wide">
             <li>
               Built and enhanced user interfaces using{" "}
@@ -57,28 +57,28 @@ export default function ExperienceSection() {
         </div>
       </div>
       <div className="w-[1px] h-[100px] bg-black mx-7  top-10 sm:rotate-0  rotate-90"></div>
-      <div className=" flex sm:flex-row flex-col items-center  w-full z-10 text-black px-7   ">
+      <div className=" z-30 flex sm:flex-row flex-col items-center  w-full z-10 text-black px-7   ">
         {/*Left side */}
         <div className=" flex flex-1  flex-col gap-7 bg-white">
           <Title className=" font-manrope font-bold">
             <a
               href="https://www.visionage-vfx.com/"
               target="_blank"
-              className=" hover:underline transition-all motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1500"
+              className=" hover:underline transition-all motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1500"
             >
               Vision Age VFX,
             </a>
             <br />
-            <span className="motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
+            <span className="motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
               React developer
             </span>
           </Title>
-          <span className=" text-sm font-manrope motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
+          <span className=" text-sm font-manrope motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600">
             Remote <br /> Sept 2025 – Nov 2025 (2 Months)
           </span>
         </div>
         {/*Right side */}
-        <div className=" flex-1 p-2 bg-white motion-translate-x-in-[-5%] motion-translate-y-in-[0%] motion-opacity-in-[0%] motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1700 ">
+        <div className=" flex-1 p-2 bg-white motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1700 ">
           <ul className=" list-disc font-manrope space-y-3 text-lg/8 tracking-wide">
             <li>
               Developed a professional networking web application inspired by
