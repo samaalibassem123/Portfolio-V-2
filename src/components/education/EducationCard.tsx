39a5b45b9ffe156ab21ef3d670db4f82f98282a6
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-10-05 21:09:00 +0100

    Update EducationCard.tsx

--- src/components/education/EducationCard.tsx
+++ src/components/education/EducationCard.tsx
@@ -11,18 +11,18 @@ export default function EducationCard({ className, education }: Props) {
   return (
     <div
       className={cn(
-        " h-fit relative pb-10 will-change-[transform,filter]  backface-hidden space-y-4 px-2",
+        " h-fit relative pb-10 will-change-[transform,filter]   backface-hidden space-y-4 px-2",
         className
       )}
     >
-      <Title className=" font-manrope font-bold motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-blur-out-0 motion-ease-spring-smooth motion-delay-1500 will-change-[transform,filter] backface-hidden translate-z-0 ">
+      <Title className=" font-manrope font-bold motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-blur-out-0 motion-ease-spring-smooth motion-delay-1500 will-change-[transform,filter] backface-hidden translate-z-0  sm:text-7xl text-5xl">
         {education.name}
       </Title>
       <div className="w-[1px] h-[100px] bg-black mx-3  top-10   rotate-0" />
-      <Title className=" font-manrope  motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600 will-change-[transform,filter] backface-hidden translate-z-0">
+      <Title className=" font-manrope  motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600 will-change-[transform,filter] backface-hidden translate-z-0 sm:text-7xl text-4xl">
         {education.schoolname}
       </Title>
-      <Title className=" font-manrope  motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600 will-change-[transform,filter] backface-hidden translate-z-0">
+      <Title className=" font-manrope  motion-translate-x-in-[-5%] motion-translate-y-in-[0%]  motion-blur-in-[5px] motion-ease-spring-smooth motion-delay-1600 will-change-[transform,filter] backface-hidden translate-z-0 sm:text-7xl text-4xl">
         {education.date}
       </Title>
     </div>
